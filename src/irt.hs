1ccfb5a1708338573fcc315b6ce2103a9f458c6f
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2011-06-08 10:19:20 +0200

    Use CmdArgs for command line argument parsing

--- src/irt.hs
+++ src/irt.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards, NamedFieldPuns #-}
 module Main where
 import System.Environment (getArgs)
 import System.IO
 import System.Process
+import System.Console.CmdArgs
 import GitRunner
+
+data CmdOptions = AboutIncl {
+                            }
+                | G4Release { gitRepositoryPath :: String
+                            , ignoreGitModifications :: Bool
+                            , geant4SourcePath :: String
+                            }
+                deriving (Show, Eq, Data, Typeable)
 
 data SourceConfig = SourceConfig { rootDir :: FilePath }
 
@@ -13,32 +23,43 @@ data SourceTransform = FileTransform { transformFileName :: FilePath
                                      }
                        deriving (Show, Eq)
 
-getGitRevisionInfo = do
-  (_, Just gitDescribeOutputStream, _, _) <- createProcess (proc "git" ["describe", "--tags"]) {std_out = CreatePipe }
-  createProcess (proc "git" ["update-index", "-q", "--refresh"])
-  (_, Just gitDiffIndexStream, _, _) <-createProcess (proc "git" ["diff-index", "--name-only", "HEAD", "--"]) {std_out = CreatePipe }
-  gitRevision <- hGetContents gitDescribeOutputStream
-  gitDiffIndex <- hGetContents gitDiffIndexStream
-  let treeIsDirty = length (lines gitDiffIndex) > 0
-      trimmedRevision = init gitRevision
-  if treeIsDirty
-    then return (trimmedRevision ++ "-dirty")
-    else return trimmedRevision
-
 parseConfig = undefined
 
 performCommand = undefined
 
-usage = do
-  let thisRepo = GitRepo "./"
-  rev <- buildGitRevisionString thisRepo
---  rev <- getGitRevisionInfo
-  putStrLn ("usage: irt version " ++ rev ++ " command [args]")
-  putStrLn "Commands: help, release"
+g4release = G4Release { gitRepositoryPath = def &= help "INCL++ Git repository path"
+                      , ignoreGitModifications = False &= help "Ignore modifications in the Git tree"
+                      , geant4SourcePath = def &= help "Geant4 checkout (main level)"
+                      }  &= help "Release INCL++ to Geant4"
+
+info = AboutIncl {} &= help "Help"
+
+mode = cmdArgsMode $ modes [info,g4release] &= help "Make an INCL release" &= program "irt" &= summary "irt v0.1"
+
+abortG4Release = do
+  putStrLn "Error! Git tree must not contain uncommitted changes!"
+
+performG4Release conf = undefined
+--performG4Release conf = do
+--  srcs <- collectF
+--  srcs <- filterSource conf
+  
+
+runIrtCommand conf@(G4Release gitpath ignoregitmodif g4sourcepath) = do
+  let inclRepository = GitRepo gitpath
+  inclDirtyTree <- gitIsDirtyTree inclRepository
+  inclRev <- buildGitRevisionString inclRepository
+  putStrLn $ "INCL++ repository path is: " ++ gitpath
+  putStrLn $ "INCL++ revision is: " ++ inclRev
+  putStrLn $ "Geant4 path is: " ++ g4sourcepath
+  if inclDirtyTree && (not ignoregitmodif)
+    then abortG4Release
+    else performG4Release conf
+
+--runIrtCommand :: CmdOptions -> IO ()
+--runIrtCommand G4Release{..} = undefined
+--runIrtCommand _ = undefined
 
 main = do
-  args <- getArgs
-  if length args <= 0
-    then usage
-    else do let config = parseConfig args
-            performCommand config
+  conf <- cmdArgsRun mode
+  runIrtCommand conf
