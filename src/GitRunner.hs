91066e8411239dc1c107717101934afaf1d39ce9
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2011-08-23 20:05:44 +0200

    Extract git error codes and error messages

--- src/GitRunner.hs
+++ src/GitRunner.hs
@@ -6,6 +6,7 @@ module GitRunner ( GitRepo(..)
   ) where
 import System.IO
 import System.Process
+import System.Exit
 
 data GitRepo = GitRepo { gitRepoPath :: FilePath
                        } deriving (Show, Eq)
@@ -23,24 +24,34 @@ gitCommandArgs GitRevisionInfoCmd = ["describe", "--tags"]
 buildGitRevisionString :: GitRepo -> IO String
 buildGitRevisionString repo = do
   isDirty <- gitIsDirtyTree repo
-  revStr <- runGit repo GitRevisionInfoCmd
+  (_, revStr) <- runGit repo GitRevisionInfoCmd
   if isDirty
-    then return (revStr ++ "-dirty")
+    then return $ revStr ++ "-dirty"
     else return revStr
 
 gitIsDirtyTree :: GitRepo -> IO Bool
 gitIsDirtyTree repo = do
-  runGit repo GitUpdateIndexCmd
-  output <- runGit repo GitDiffIndexNamesCmd
+  (_, _) <- runGit repo GitUpdateIndexCmd
+  (_, output) <- runGit repo GitDiffIndexNamesCmd
   let changedFiles = length output
-  return (changedFiles > 0)
+  return $ changedFiles > 0
 
-runGit :: GitRepo -> GitCommand -> IO String
+runGit :: GitRepo -> GitCommand -> IO (ExitCode, String)
 runGit repo command = do
   let gitArgs = gitCommandArgs command
       repoDir = gitRepoPath repo
-  (_, Just gitOutputStream, _, _) <- createProcess (proc "git" gitArgs) {std_out = CreatePipe,cwd = Just repoDir}
-  gitOutputStr <- hGetContents gitOutputStream
-  let trimmedOutput | length gitOutputStr > 0 = init gitOutputStr
-                    | otherwise               = gitOutputStr
-  return trimmedOutput
+      out = CreatePipe
+      err = CreatePipe
+      d = Just repoDir
+  (_, Just gitOutputStream, Just gitErrorStream, procHandle) <- createProcess (proc "git" gitArgs) {std_out = out, std_err = err,cwd = d}
+  exitCode <- waitForProcess procHandle
+  case exitCode of
+    ExitSuccess -> do
+      gitOutputStr <- hGetContents gitOutputStream
+      let trimmedOutput | length gitOutputStr > 0 = init gitOutputStr
+                        | otherwise               = gitOutputStr
+      return (ExitSuccess, trimmedOutput)
+    (ExitFailure errorCode) -> do
+      gitErrorStr  <- hGetContents gitErrorStream
+      let msg = "Git failed with code " ++ (show errorCode) ++ "\n" ++ gitErrorStr
+      return (exitCode, msg)
